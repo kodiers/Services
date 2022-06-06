62e10233a131c8d08919d217ca896caeb0f9bcb5
Victor Yamchinov <kodiers@gmail.com>
2022-06-07 00:20:57 +0300

    added simple service

--- app/src/main/java/com/tfl/services/MainActivity.kt
+++ app/src/main/java/com/tfl/services/MainActivity.kt
@@ -2,10 +2,19 @@ package com.tfl.services
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tfl.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
     }
 }
